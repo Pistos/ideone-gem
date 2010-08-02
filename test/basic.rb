03c42e27574e4ca66123bdd3e6648b56036b4fea
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-08-01 23:42:27 -0400

    Grammar adjustment in specs.

--- test/basic.rb
+++ test/basic.rb
@@ -1,7 +1,7 @@
 require 'ideone'
 require 'bacon'
 
-describe 'ideone gem user' do
+describe 'an ideone gem user' do
   it 'can submit Ruby code and receive stdout' do
     paste_id = Ideone.submit( :ruby, %{puts "text on stdout"} )
     results = Ideone.run( paste_id, nil )
